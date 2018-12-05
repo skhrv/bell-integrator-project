657221953f48101ab87dda6aef00b140a66098dd
axis <axis@localhost.localdomain>
2018-12-05 19:12:32 +0500

    add reduces for Action Companies

--- app/reducers/index.ts
+++ app/reducers/index.ts
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
-import { Login, Logout } from '../actions/consts';
+import { reducer as formReducer } from 'redux-form';
+import { ICompany } from '../actions';
+import { AddCompany, CompaniesFetch, Login, Logout, RemoveCompany } from '../actions/consts';
 
 export interface IStoreState {
   loginStatus: boolean;
   loading: boolean;
   error: string | null;
+  companies: ICompany[];
 }
 
 export interface IAction {
@@ -36,16 +39,28 @@ const loading = (state: boolean = false, action: IAction) => {
       return false;
     case Logout.REQUEST:
       return true;
+    case AddCompany.REQUEST:
+      return true;
+    case AddCompany.SUCCESS:
+      return false;
+    case AddCompany.FAILURE:
+      return false;
+    case CompaniesFetch.REQUEST:
+      return true;
+    case CompaniesFetch.SUCCESS:
+      return false;
+    case CompaniesFetch.FAILURE:
+      return false;
   }
   return state;
 };
 
-const error = (state: string | null = null, action: IAction) => {
+const error = (state: string = null, action: IAction) => {
   switch (action.type) {
     case Login.FAILURE:
       return action.payload;
     case Logout.FAILURE:
-      return true;
+      return action.payload;
     case Login.REQUEST:
       return null;
     case Logout.REQUEST:
@@ -54,8 +69,32 @@ const error = (state: string | null = null, action: IAction) => {
       return null;
     case Logout.SUCCESS:
       return null;
+    case CompaniesFetch.SUCCESS:
+      return null;
+    case AddCompany.SUCCESS:
+      return null;
+    case RemoveCompany.SUCCESS:
+      return null;
+    case CompaniesFetch.FAILURE:
+      return action.payload;
+    case AddCompany.FAILURE:
+      return action.payload;
+    case RemoveCompany.FAILURE:
+      return action.payload;
   }
   return state;
 };
 
-export default combineReducers({ loginStatus, loading, error });
+const companies = (state: ICompany[] = [], action: IAction) => {
+  switch (action.type) {
+    case CompaniesFetch.SUCCESS:
+      return action.payload;
+    case AddCompany.SUCCESS:
+      return [...state, action.payload];
+    case RemoveCompany.SUCCESS:
+      return state.filter(({ id }) => id !== action.payload.id);
+  }
+  return state;
+};
+
+export default combineReducers({ loginStatus, loading, error, companies, form: formReducer });
