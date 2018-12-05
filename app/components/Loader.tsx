8d382e097ee6123b3dfdf6274ba588cfe05b37fc
axis <axis@localhost.localdomain>
2018-12-05 19:12:46 +0500

    add Loader

---
+++ app/components/Loader.tsx
@@ -0,0 +1,12 @@
+import 'loaders.css';
+import * as React from 'react';
+
+const loader = () => (
+  <div className="loader-inner ball-pulse d-flex justify-content-center">
+    <div className="bg-primary" />
+    <div className="bg-primary" />
+    <div className="bg-primary" />
+  </div>
+);
+
+export { loader as Loader };
