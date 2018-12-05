c29f7a3a8b215f27bf633fe4af09e9f25d3a3e43
axis <axis@localhost.localdomain>
2018-12-05 19:00:13 +0500

    add actions for Companies

--- app/actions/index.ts
+++ app/actions/index.ts
@@ -1,29 +1,32 @@
 import axios from 'axios';
 import { Dispatch } from 'redux';
 import { createAsyncAction } from 'typesafe-actions';
-import { Login, Logout } from './consts';
+import { AddCompany, CompaniesFetch, Login, Logout, RemoveCompany } from './consts';
+import routes from './routes';
 
-export interface IUserPayload {
+export interface IUser {
   email: string;
   password: string;
+}
+
+export interface ICompany {
+  id?: string;
+  name: string;
+  inn: number;
+  address: string;
 }
 
 const login = createAsyncAction(
   Login.REQUEST,
   Login.SUCCESS,
   Login.FAILURE,
-)<void, IUserPayload, Error>();
+)<void, IUser, Error>();
 
-const corsProxy = 'https://cors-anywhere.herokuapp.com/';
-
-export const onLogin = (user: IUserPayload) => async (dispatch: Dispatch) => {
+export const onLogin = (user: IUser) => async (dispatch: Dispatch) => {
   dispatch(login.request());
   const urlLogin = 'http://www.mocky.io/v2/5aafaf6f2d000057006eff31';
-
-  const urlWithProxy = `${corsProxy}${urlLogin}`;
   try {
-    const response = await axios.post(urlWithProxy, user);
-
+    const response = await axios.post(urlLogin, user);
     dispatch(login.success(response.data));
   } catch (e) {
     dispatch(login.failure(e.message));
@@ -34,18 +37,63 @@ const logout = createAsyncAction(
   Logout.REQUEST,
   Logout.SUCCESS,
   Logout.FAILURE,
-)<void, IUserPayload, Error>();
+)<void, IUser, Error>();
 
 export const onLogout = () => async (dispatch: Dispatch) => {
   dispatch(logout.request());
   const urlLogout = 'http://www.mocky.io/v2/5aafaf6f2d000057006eff31';
-
-  const urlWithProxy = `${corsProxy}${urlLogout}`;
   try {
-    const response = await axios.get(urlWithProxy);
-
+    const response = await axios.post(urlLogout);
     dispatch(logout.success(response.data));
   } catch (e) {
     dispatch(logout.failure(e.message));
+  }
+};
+
+const companiesFetch = createAsyncAction(
+  CompaniesFetch.REQUEST,
+  CompaniesFetch.SUCCESS,
+  CompaniesFetch.FAILURE,
+)<void, ICompany, Error>();
+
+export const onCompaniesFetch = () => async (dispatch: Dispatch) => {
+  dispatch(companiesFetch.request());
+  try {
+    const response = await axios.get(routes.companiesUrl());
+    dispatch(companiesFetch.success(response.data));
+  } catch (e) {
+    dispatch(companiesFetch.failure(e.message));
+  }
+};
+
+const addCompany = createAsyncAction(
+  AddCompany.REQUEST,
+  AddCompany.SUCCESS,
+  AddCompany.FAILURE,
+)<void, ICompany, Error>();
+
+export const onAddCompany = (company: ICompany) => async (dispatch: Dispatch) => {
+  dispatch(addCompany.request());
+  try {
+    const response = await axios.post(routes.companiesUrl(), company);
+    dispatch(addCompany.success(response.data));
+  } catch (e) {
+    dispatch(addCompany.failure(e.message));
+  }
+};
+
+const removeCompany = createAsyncAction(
+  RemoveCompany.REQUEST,
+  RemoveCompany.SUCCESS,
+  RemoveCompany.FAILURE,
+)<void, ICompany, Error>();
+
+export const onRemoveCompany = (id: string) => async (dispatch: Dispatch) => {
+  dispatch(removeCompany.request());
+  try {
+    const response = await axios.delete(routes.companyUrl(id));
+    dispatch(removeCompany.success(response.data));
+  } catch (e) {
+    dispatch(removeCompany.failure(e.message));
   }
 };
---
+++ app/actions/routes.ts
@@ -0,0 +1,20 @@
+const host = 'https://bell-json-server.herokuapp.com';
+// const host = 'http://localhost:3000'; // local json server
+// Create new Company
+// POST /companies
+//    {
+//     "name": "ООО Софа",
+//     "inn": "0276097173",
+//     "address": "г.Уфа, ул.Менделеева 178"
+//   }
+
+// Get List Of Companies
+// GET /companies
+
+// Get Company
+// GET /companies/:id
+
+export default {
+  companiesUrl: () => [host, 'companies'].join('/'), // get companies list
+  companyUrl: (id: string) => [host, 'companies', id].join('/'),
+};
