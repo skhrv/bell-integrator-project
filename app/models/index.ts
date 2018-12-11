7add17fbfdefa4445cbed8e2ece0502a1998d2c5
skhrv <notebook1v@gmail.com>
2018-12-11 13:51:08 +0500

    tranfser models to /models

---
+++ app/models/index.ts
@@ -0,0 +1,60 @@
+export interface IUser {
+  email: string;
+  password: string;
+}
+
+export interface ICompany {
+  id?: string;
+  name: string;
+  inn: number;
+  address: string;
+}
+
+export interface IEmployee {
+  id?: string;
+  subDivisionId: string;
+  fullname: string;
+  phone: string;
+  address: string;
+  position: string;
+}
+
+export interface ISubDivision {
+  id?: string;
+  companyId: string;
+  name: string;
+  phone: string;
+}
+
+export type Item = ICompany | ISubDivision | IEmployee;
+
+export interface IModalState {
+  open: boolean;
+  mode: string;
+  currentItemEdit: ICompany | ISubDivision | IEmployee;
+}
+
+export interface IPropsList {
+  error: string;
+  loading: boolean;
+  openModal: () => void;
+  closeModal: () => void;
+  openEditModal: (payload: Item) => void;
+  openAddModal: () => void;
+  modal: IModalState;
+}
+
+export interface IStoreState {
+  companies: ICompany[];
+  employees: IEmployee[];
+  error: string | null;
+  loading: boolean;
+  loginStatus: boolean;
+  modal: IModalState;
+  subDivisions: ISubDivision[];
+}
+
+export interface IAction {
+  type: string;
+  payload?: any;
+}
